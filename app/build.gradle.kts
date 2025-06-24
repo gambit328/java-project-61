88ad142f1e06bcda128cd32e2dedc2bd442efc5b
gambit328 <darklar@mail.ru>
2025-06-24 19:52:15 +0700

    configured source project

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    id("application")
+    id("com.github.ben-manes.versions") version "0.52.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.13.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
