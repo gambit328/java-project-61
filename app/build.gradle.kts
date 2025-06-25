86d31817d50f9fc3a4193cd8ffbd31723cf8fcea
gambit328 <darklar@mail.ru>
2025-06-25 21:32:16 +0700

    fix checkstyle + added working directory for sonarqube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("java")
     id("application")
-    id("com.github.ben-manes.versions") version "0.52.0"
     id("checkstyle")
+    id("com.github.ben-manes.versions") version "0.52.0"
     id("org.sonarqube") version "6.2.0.5505"
 }
 
@@ -22,15 +22,12 @@ application {
     mainClass.set("hexlet.code.App")
 }
 
-checkstyle {
-    configFile = file("config/checkstyle/checkstyle.xml")
-}
-
 sonar {
     properties {
         property("sonar.projectKey", "gambit328_java-project-61")
         property("sonar.organization", "gambit328")
         property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.working.directory", "/app")
     }
 }
 
